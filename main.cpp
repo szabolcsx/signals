5b21f6c469d2604bf15ad6d84b34e6c1ac434eef
Szőke Szabolcs <szabolcsx95@gmail.com>
2015-11-23 05:11:55 +0200

    Added Visual C++ project

--- main.cpp
+++ main.cpp
@@ -5,104 +5,104 @@
 
 void func(int x)
 {
-  std::cout << "void func(" << x << ")" << std::endl;
+	std::cout << "void func(" << x << ")" << std::endl;
 }
 
 class foo : public szabi::signals::auto_disconnect
 {
 public:
-  foo() {}
-  ~foo() {}
+	foo() {}
+	~foo() {}
 
-  void func(int x)
-  {
-    std::cout << "void foo::func(" << x << ")" << std::endl;
-  }
+	void func(int x)
+	{
+		std::cout << "void foo::func(" << x << ")" << std::endl;
+	}
 
-  void func(int x, int y)
-  {
-    std::cout << "void foo::func(" << x << ", " << y << ")" << std::endl;
-  }
+	void func(int x, int y)
+	{
+		std::cout << "void foo::func(" << x << ", " << y << ")" << std::endl;
+	}
 
-  void func0()
-  {
-    std::cout << "void foo::func0()" << std::endl;
-  }
+	void func0()
+	{
+		std::cout << "void foo::func0()" << std::endl;
+	}
 
-  void func1(int x)
-  {
-    std::cout << "void foo::func1(" << x << ")" << std::endl;
-  }
+	void func1(int x)
+	{
+		std::cout << "void foo::func1(" << x << ")" << std::endl;
+	}
 
-  void func2(int x, int y)
-  {
-    std::cout << "void foo::func2(" << x << ", " << y << ")" << std::endl;
-  }
+	void func2(int x, int y)
+	{
+		std::cout << "void foo::func2(" << x << ", " << y << ")" << std::endl;
+	}
 };
 
 class bad : public szabi::signals::auto_disconnect
 {
 public:
-  bad()
-  {
-    std::cout << "bad constructed" << std::endl;
-  }
-  ~bad()
-  {
-    std::cout << "bad destructed" << std::endl;
-  }
+	bad()
+	{
+		std::cout << "bad constructed" << std::endl;
+	}
+	~bad()
+	{
+		std::cout << "bad destructed" << std::endl;
+	}
 
-  void func(int)
-  {
-    std::cout << std::endl << "void bad::func()" << std::endl << std::endl;
-  }
+	void func(int)
+	{
+		std::cout << std::endl << "void bad::func()" << std::endl << std::endl;
+	}
 };
 
 void should_not_fire()
 {
-  std::cout << "void should_not_fire()" << std::endl;
+	std::cout << "void should_not_fire()" << std::endl;
 }
 
 int main()
 {
-  szabi::signal<> signal0;
-  szabi::signal<int> signal1;
-  szabi::signal<int, int> signal2;
-  foo f;
-  signal0.connect(&foo::func0, f);
-  signal1.connect([](int x)
-  {
-    std::cout << "Lambda func, x = " << x << std::endl;
-  });
+	szabi::signal<> signal0;
+	szabi::signal<int> signal1;
+	szabi::signal<int, int> signal2;
+	foo f;
+	signal0.connect(&foo::func0, f);
+	signal1.connect([](int x)
+	{
+		std::cout << "Lambda func, x = " << x << std::endl;
+	});
 
-  signal1.connect(&func);
+	signal1.connect(&func);
 
-  // overloaded slots
-  signal1.connect(static_cast<void(foo::*)(int)>(&foo::func), f);
-  signal2.connect(static_cast<void(foo::*)(int, int)>(&foo::func), f);
-  // using overload<...>::of(&foo::bar) helper
-  signal1.connect(szabi::overload<int>::of(&foo::func), f);
-  signal2.connect(szabi::overload<int, int>::of(&foo::func), f);
+	// overloaded slots
+	signal1.connect(static_cast<void(foo::*)(int)>(&foo::func), f);
+	signal2.connect(static_cast<void(foo::*)(int, int)>(&foo::func), f);
+	// using overload<...>::of(&foo::bar) helper
+	signal1.connect(szabi::overload<int>::of(&foo::func), f);
+	signal2.connect(szabi::overload<int, int>::of(&foo::func), f);
 
-  signal1.connect(&foo::func1, &f);
-  signal2.connect(&foo::func2, &f);
+	signal1.connect(&foo::func1, &f);
+	signal2.connect(&foo::func2, &f);
 
-  {
-    bad b;
-    auto conn = signal0.connect(&should_not_fire);
-    conn.disconnect();
+	{
+		bad b;
+		auto conn = signal0.connect(&should_not_fire);
+		conn.disconnect();
 
-    if(!conn.connected())
-    {
-      std::cout << "disconnected" << std::endl;
-    }
+		if (!conn.connected())
+		{
+			std::cout << "disconnected" << std::endl;
+		}
 
-    signal1.connect(&bad::func, b);
-  }
+		signal1.connect(&bad::func, b);
+	}
 
-  signal0();
-  signal1(1);
-  signal2(1, 2);
+	signal0();
+	signal1(1);
+	signal2(1, 2);
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
--- signal.h
+++ signal.h
@@ -8,183 +8,183 @@
 
 namespace szabi
 {
-  // When a slot is overloaded an ugly cast should be used
-  // static_cast<void(T::*)(<parameters>)>(&T::<slot>);
-  // This helper simplifies this a bit
-  // overload<parameters>::of(&T::slot);
-  template <typename... Args>
-  struct overload
-  {
-    template <typename F, typename R>
-    static constexpr auto of(R(F::*ptr)(Args...)) -> decltype(ptr)
-    {
-      return ptr;
-    }
-  };
+	// When a slot is overloaded an ugly cast should be used
+	// static_cast<void(T::*)(<parameters>)>(&T::<slot>);
+	// This helper simplifies this a bit
+	// overload<parameters>::of(&T::slot);
+	template <typename... Args>
+	struct overload
+	{
+		template <typename F, typename R>
+		static constexpr auto of(R(F::*ptr)(Args...)) -> decltype(ptr)
+		{
+			return ptr;
+		}
+	};
 
-  namespace
-  {
-    class slot_base
-    {
-    public:
-      virtual void disconnect() = 0;
-    };
+	namespace
+	{
+		class slot_base
+		{
+		public:
+			virtual void disconnect() = 0;
+		};
 
-    template <typename... Args>
-    class slot_impl : public slot_base
-    {
-    public:
-      using slot_type = std::function<void(Args...)>;
-      using disconnector_type = std::function<void()>;
+		template <typename... Args>
+		class slot_impl : public slot_base
+		{
+		public:
+			using slot_type = std::function<void(Args...)>;
+			using disconnector_type = std::function<void()>;
 
-      slot_impl(slot_type&& slot, disconnector_type&& disconnector) :
-        slot(std::move(slot)),
-        disconnector(std::move(disconnector))
-      {}
+			slot_impl(slot_type&& slot, disconnector_type&& disconnector) :
+				slot(std::move(slot)),
+				disconnector(std::move(disconnector))
+			{}
 
-      void disconnect()
-      {
-        this->disconnector();
-      }
+			void disconnect()
+			{
+				this->disconnector();
+			}
 
-      void operator()(Args... args)
-      {
-        this->slot(std::forward<Args>(args)...);
-      }
+			void operator()(Args... args)
+			{
+				this->slot(std::forward<Args>(args)...);
+			}
 
-      private:
-        slot_type slot;
-        disconnector_type disconnector;
-    };
-  }
+		private:
+			slot_type slot;
+			disconnector_type disconnector;
+		};
+	}
 
-  // This class manages a connection between a signal and slot
-  class connection
-  {
-  public:
-    connection(const std::weak_ptr<slot_base>& slot) : slot(slot) {}
+	// This class manages a connection between a signal and slot
+	class connection
+	{
+	public:
+		connection(const std::weak_ptr<slot_base>& slot) : slot(slot) {}
 
-    ~connection() {}
+		~connection() {}
 
-    void disconnect() const
-    {
-      if(auto temp = this->slot.lock())
-      {
-        temp->disconnect();
-      }
-    }
+		void disconnect() const
+		{
+			if (auto temp = this->slot.lock())
+			{
+				temp->disconnect();
+			}
+		}
 
-    bool connected() const
-    {
-      return !this->slot.expired();
-    }
+		bool connected() const
+		{
+			return !this->slot.expired();
+		}
 
-  private:
-    std::weak_ptr<slot_base> slot;
-  };
+	private:
+		std::weak_ptr<slot_base> slot;
+	};
 
-  template <typename... Args>
-  class signal;
+	template <typename... Args>
+	class signal;
 
-  namespace signals
-  {
-    // This class is used to disconnect all slots when the object is destroyed
-    class auto_disconnect
-    {
-      // A signal has access to add_connection function
-      template <typename... Args> friend class szabi::signal;
-    public:
-      virtual ~auto_disconnect()
-      {
-        std::lock_guard<std::mutex> lock(this->mutex);
-        for(const auto& conn : this->connections)
-        {
-          conn.disconnect();
-        }
-      }
+	namespace signals
+	{
+		// This class is used to disconnect all slots when the object is destroyed
+		class auto_disconnect
+		{
+			// A signal has access to add_connection function
+			template <typename... Args> friend class szabi::signal;
+		public:
+			virtual ~auto_disconnect()
+			{
+				std::lock_guard<std::mutex> lock(this->mutex);
+				for (const auto& conn : this->connections)
+				{
+					conn.disconnect();
+				}
+			}
 
-    protected:
-      void add_connection(const connection& conn)
-      {
-        std::lock_guard<std::mutex> lock(this->mutex);
-        this->connections.push_back(conn);
-      }
+		protected:
+			void add_connection(const connection& conn)
+			{
+				std::lock_guard<std::mutex> lock(this->mutex);
+				this->connections.push_back(conn);
+			}
 
-    private:
-      std::vector<connection> connections;
-      mutable std::mutex mutex;
-    };
-  }
+		private:
+			std::vector<connection> connections;
+			mutable std::mutex mutex;
+		};
+	}
 
-  template <typename... Args>
-  class signal
-  {
-  public:
-    signal() {}
-    virtual ~signal() {}
+	template <typename... Args>
+	class signal
+	{
+	public:
+		signal() {}
+		virtual ~signal() {}
 
-    using slot_type = slot_impl<Args...>;
-    using slot_container_type = std::vector<std::shared_ptr<slot_type>>;
-    using slot_iterator_type = typename slot_container_type::iterator;
+		using slot_type = slot_impl<Args...>;
+		using slot_container_type = std::vector<std::shared_ptr<slot_type>>;
+		using slot_iterator_type = typename slot_container_type::iterator;
 
-    template <typename S>
-    connection connect(S&& slot)
-    {
-      return this->connect_impl(slot);
-    }
+		template <typename S>
+		connection connect(S&& slot)
+		{
+			return this->connect_impl(slot);
+		}
 
-    template <typename S, typename T>
-    connection connect(S&& slot, T* instance)
-    {
-      static_assert(std::is_base_of<signals::auto_disconnect, T>::value, "T must inherit auto_disconnect");
-      connection conn = this->connect_impl(
-        [&](Args... args)
-        {
-          // Using a lambda function to call a slot which is a member function
-          (static_cast<T*>(instance)->*slot)(std::forward<Args>(args)...);
-        });
+		template <typename S, typename T>
+		connection connect(S&& slot, T* instance)
+		{
+			static_assert(std::is_base_of<signals::auto_disconnect, T>::value, "T must inherit auto_disconnect");
+			connection conn = this->connect_impl(
+				[&](Args... args)
+			{
+				// Using a lambda function to call a slot which is a member function
+				(static_cast<T*>(instance)->*slot)(std::forward<Args>(args)...);
+			});
 
-      instance->add_connection(conn);
+			instance->add_connection(conn);
 
-      return conn;
-    }
+			return conn;
+		}
 
-    template <typename S, typename T>
-    connection connect(S&& slot, T& instance)
-    {
-      return this->connect(slot, std::addressof(instance));
-    }
+		template <typename S, typename T>
+		connection connect(S&& slot, T& instance)
+		{
+			return this->connect(slot, std::addressof(instance));
+		}
 
-    void operator()(Args const&... args)
-    {
-      std::lock_guard<std::mutex> lock(this->mutex);
-      for(auto const& slot : this->slots)
-      {
-        if(slot)
-        {
-          (*slot)(args...);
-        }
-      }
-    }
-  private:
-    slot_container_type slots;
-    mutable std::mutex mutex;
+		void operator()(Args const&... args)
+		{
+			std::lock_guard<std::mutex> lock(this->mutex);
+			for (auto const& slot : this->slots)
+			{
+				if (slot)
+				{
+					(*slot)(args...);
+				}
+			}
+		}
+	private:
+		slot_container_type slots;
+		mutable std::mutex mutex;
 
-    template<typename S>
-    connection connect_impl(S&& slot)
-    {
-      std::lock_guard<std::mutex> lock(this->mutex);
-      this->slots.push_back(std::shared_ptr<slot_type>(new slot_type(
-        std::forward<S>(slot), [&]()
-        {
-          std::lock_guard<std::mutex> lock(this->mutex);
-          slot_iterator_type it = std::prev(this->slots.end());
-          this->slots.erase(it);
-        })));
+		template<typename S>
+		connection connect_impl(S&& slot)
+		{
+			std::lock_guard<std::mutex> lock(this->mutex);
+			this->slots.push_back(std::shared_ptr<slot_type>(new slot_type(
+				std::forward<S>(slot), [&]()
+			{
+				std::lock_guard<std::mutex> lock(this->mutex);
+				slot_iterator_type it = std::prev(this->slots.end());
+				this->slots.erase(it);
+			})));
 
-      return { this->slots.back() };
-    }
-  };
+			return{ this->slots.back() };
+		}
+	};
 }
 
 #endif /* SIGNAL_H__ */
