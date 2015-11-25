18980a26e39f744bfb94317b6a059069a9d3b8b0
Szőke Szabolcs <szabolcsx95@gmail.com>
2015-11-26 01:08:18 +0200

    Updated tests

--- test.cpp
+++ test.cpp
@@ -1,95 +1,116 @@
 #include <signals.h>
 
 #include <iostream>
+#include <string>
 #include <cstdlib>
 
-void func(int x) {
-    std::cout << "void func(" << x << ")" << std::endl;
+void slot(const std::string& message) {
+    std::cout << message << std::endl << std::endl;
 }
 
-class foo : public szabi::signals::auto_disconnect {
+void slot_with_ref(std::string& message) {
+    message = "The slot was executed";
+}
+
+void slot_should_be_disconnected() {
+    std::cout << "This should not be seen" << std::endl << std::endl;
+}
+
+void overloaded_slot(const std::string& message) {
+    slot(message);
+}
+
+void overloaded_slot(const std::string& message1, const std::string& message2) {
+    std::cout << message1 << " " << message2 << std::endl << std::endl;
+}
+
+class slots : public szabi::signals::auto_disconnect {
 public:
-    foo() { }
+    slots() { }
 
-    ~foo() { }
+    ~slots() { }
 
-    void func(int x) {
-        std::cout << "void foo::func(" << x << ")" << std::endl;
+    void slot(const std::string& message) {
+        std::cout << message << std::endl << std::endl;
     }
 
-    void func(int x, int y) {
-        std::cout << "void foo::func(" << x << ", " << y << ")" << std::endl;
+    void overloaded_slot(const std::string& message) {
+        this->slot(message);
     }
 
-    void func0() {
-        std::cout << "void foo::func0()" << std::endl;
+    void overloaded_slot(const std::string& message1, const std::string& message2) {
+        std::cout << message1 << " " << message2 << std::endl << std::endl;
     }
 
-    void func1(int x) {
-        std::cout << "void foo::func1(" << x << ")" << std::endl;
+    void slot0() {
+        std::cout << "Hello world" << std::endl << std::endl;
     }
 
-    void func2(int x, int y) {
-        std::cout << "void foo::func2(" << x << ", " << y << ")" << std::endl;
+    void slot1(const std::string& message) {
+        this->slot(message);
+    }
+
+    void slot2(const std::string& message1, const std::string& message2) {
+        std::cout << message1 << " " << message2 << std::endl << std::endl;
     }
 };
 
-class bad : public szabi::signals::auto_disconnect {
+class this_should_not_be_seen : public szabi::signals::auto_disconnect {
 public:
-    bad() {
-        std::cout << "bad constructed" << std::endl;
-    }
+    this_should_not_be_seen() {}
+    ~this_should_not_be_seen() {}
 
-    ~bad() {
-        std::cout << "bad destructed" << std::endl;
-    }
-
-    void func(int) {
-        std::cout << std::endl << "void bad::func()" << std::endl << std::endl;
+    void slot_should_be_disconnected() {
+        std::cout << "This should not be seen" << std::endl << std::endl;
     }
 };
-
-void should_not_fire() {
-    std::cout << "void should_not_fire()" << std::endl;
-}
 
 int main() {
     szabi::signal<> signal0;
-    szabi::signal<int> signal1;
-    szabi::signal<int, int> signal2;
-    foo f;
-    signal0.connect(&foo::func0, f);
-    signal1.connect([](int x) {
-        std::cout << "Lambda func, x = " << x << std::endl;
+    szabi::signal<const std::string&> signal1;
+    szabi::signal<const std::string&, const std::string&> signal2;
+    szabi::signal<std::string&> signal_ref;
+
+    signal_ref.connect(&slot_with_ref);
+
+    slots s;
+    signal0.connect(&slots::slot0, s);
+
+    signal1.connect([](const std::string& message) {
+        std::cout << message << std::endl << std::endl;
     });
 
-    signal1.connect(&func);
-
     // overloaded slots
-    signal1.connect(static_cast<void (foo::*)(int)>(&foo::func), f);
-    signal2.connect(static_cast<void (foo::*)(int, int)>(&foo::func), f);
-    // using overload<...>::of(&foo::bar) helper
-    signal1.connect(szabi::overload<int>::of(&foo::func), f);
-    signal2.connect(szabi::overload<int, int>::of(&foo::func), f);
+    signal1.connect(szabi::overload<std::string>::of(&overloaded_slot));
+    signal2.connect(szabi::overload<std::string, std::string>::of(&overloaded_slot));
 
-    signal1.connect(&foo::func1, &f);
-    signal2.connect(&foo::func2, &f);
+    // using overload<...>::of(&foo::bar) helper
+    signal1.connect(szabi::overload<std::string>::of(&slots::overloaded_slot), s);
+    signal2.connect(szabi::overload<std::string, std::string>::of(&slots::overloaded_slot), s);
+
+    signal1.connect(&slots::slot1, &s);
+    signal2.connect(&slots::slot2, &s);
+
+    auto conn0 = signal0.connect(&slot_should_be_disconnected);
+    conn0.disconnect();
+
+    if (!conn0.connected()) {
+        std::cout << "Slot disconnected" << std::endl << std::endl;
+    }
 
     {
-        bad b;
-        auto conn0 = signal0.connect(&should_not_fire);
-        conn0.disconnect();
-
-        if (!conn0.connected()) {
-            std::cout << "disconnected" << std::endl;
-        }
-
-        szabi::signals::connection conn1 = signal1.connect(&bad::func, b);
+        this_should_not_be_seen should_not_seen;
+        szabi::signals::connection conn1 = signal0.connect(&this_should_not_be_seen::slot_should_be_disconnected, should_not_seen);
     }
 
     signal0.emit();
-    signal1.emit(1);
-    signal2.emit(1, 2);
+    signal1.emit("Message one");
+    signal2.emit("Message one", "Message two");
+
+    std::string message = "The slot wasn't executed";
+    signal_ref.emit(message);
+
+    std::cout << message << std::endl << std::endl;
 
     return EXIT_SUCCESS;
 }
